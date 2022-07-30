a93c8ad60d9c265783e366f38ff332298808632f
ankitjangra@turningCloud.com <ankitjangra@turningCloud.com>
2022-07-30 22:28:49 +0530

    Done

--- index.js
+++ index.js
@@ -23,4 +23,8 @@ app.use('/users',userRoutes)
 
 const PORT = process.env.PORT || 5000;
 
+app.get('/',(req,res)=>{
+    res.send("Hello from Ankit")
+})
+
 app.listen(PORT,()=>{console.log(`Server is running at port ${PORT}`)})
